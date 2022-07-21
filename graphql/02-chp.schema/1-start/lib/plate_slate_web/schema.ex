90906ba4b5758c984c96cc1b88b7f36b172e398a
Leandro Marques <leandrolmarques@gmail.com>
2022-07-21 19:50:13 -0300

    add menu_items field to root query

--- graphql/02-chp.schema/1-start/lib/plate_slate_web/schema.ex
+++ graphql/02-chp.schema/1-start/lib/plate_slate_web/schema.ex
@@ -9,11 +9,19 @@
 defmodule PlateSlateWeb.Schema do
   use Absinthe.Schema
 
+  alias PlateSlate.{Menu, Repo}
+
   query do
-    # <<Ignore this for now>>
+    field :menu_items, list_of(:menu_item) do
+      resolve fn _, _, _ ->
+        {:ok, Repo.all(Menu.Item)}
+      end
+    end
   end
 
   object :menu_item do
-    # <<We'll add fields soon>>
+    field :id, :id
+    field :name, :string
+    field :description, :string
   end
 end
