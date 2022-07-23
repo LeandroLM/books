a5d6b4195731ce3a83854edcabb58070c7d41917
Leandro Marques <leandrolmarques@gmail.com>
2022-07-23 13:34:53 -0300

    add matching parameter to menu items

--- graphql/02-chp.schema/1-start/lib/plate_slate_web/schema.ex
+++ graphql/02-chp.schema/1-start/lib/plate_slate_web/schema.ex
@@ -10,12 +10,18 @@ defmodule PlateSlateWeb.Schema do
   use Absinthe.Schema
 
   alias PlateSlate.{Menu, Repo}
+  import Ecto.Query
 
   query do
     @desc "The list of available items on the menu"
     field :menu_items, list_of(:menu_item) do
-      resolve fn _, _, _ ->
-        {:ok, Repo.all(Menu.Item)}
+      arg :matching, :string
+      resolve fn
+        _, %{matching: name}, _ when is_binary(name) ->
+          query = from t in Menu.Item, where: ilike(t.name, ^"%#{name}%")
+          {:ok, Repo.all(query)}
+        _, _, _ ->
+          {:ok, Repo.all(Menu.Item)}
       end
     end
   end
