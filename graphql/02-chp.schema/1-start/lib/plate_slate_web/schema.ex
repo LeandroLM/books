84994096549792c686aabe80295a0a25518323bd
Leandro Marques <leandrolmarques@gmail.com>
2022-08-15 08:00:45 -0300

    add allergy info to menu item

--- graphql/02-chp.schema/1-start/lib/plate_slate_web/schema.ex
+++ graphql/02-chp.schema/1-start/lib/plate_slate_web/schema.ex
@@ -15,6 +15,11 @@ defmodule PlateSlateWeb.Schema do
   import_types __MODULE__.MenuTypes
   import_types __MODULE__.OrderingTypes
 
+  def middleware(middleware, field, %{identifier: :allergy_info} = object) do
+    new_middleware = {Absinthe.Middleware.MapGet, to_string(field.identifier)}
+    Absinthe.Schema.replace_default(middleware, new_middleware, field, object)
+  end
+
   def middleware(middleware, _field, %{identifier: :mutation}) do
     middleware ++ [Middleware.ChangesetErrors]
   end
